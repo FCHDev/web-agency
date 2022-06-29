6a2ddb93f250bae7a599e887b3bc870d9182e8ec
FCHDev <102167413+FCHDev@users.noreply.github.com>
2022-06-29 16:35:23 +0200

    Feature : ajout composant DynamicText pour la homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,26 @@
 import React from "react";
 import Navigation from "../components/Navigation";
 import Mouse from "../components/Mouse";
+import SocialNetwork from "../components/SocialNetwork";
+import DynamicText from "../components/DynamicText";
 
 const Home = () => {
+  console.log("je log Home");
   return (
     <div>
       <Mouse />
-      <Navigation />
+      <div className="home">
+        <Navigation />
+        <SocialNetwork />
+        <div className="home-main">
+          <div className="main-content">
+            <h1>FCH AGENCY</h1>
+            <h2>
+              <DynamicText />
+            </h2>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
