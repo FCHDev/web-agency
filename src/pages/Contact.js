6264a0dd1f77e6b7c875670c26fbf2eb255383d4
FCHDev <102167413+FCHDev@users.noreply.github.com>
2022-07-15 13:18:37 +0200

    Ajout pages projets Ajout ContactForm Ajout Animations Commit final

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,10 +1,87 @@
 import React from "react";
+import Mouse from "../components/Mouse";
+import Navigation from "../components/Navigation";
+import Logo from "../components/Logo";
+import ContactForm from "../components/ContactForm";
+import { CopyToClipboard } from "react-copy-to-clipboard";
+import SocialNetwork from "../components/SocialNetwork";
+import Buttons from "../components/Buttons";
+import { motion } from "framer-motion";
 
 const Contact = () => {
+  const pageTransition = {
+    in: {
+      opacity: 1,
+      x: 0,
+    },
+    out: {
+      opacity: 0,
+      x: 200,
+    },
+  };
+
   return (
-    <div>
-      <h1>Contact</h1>
-    </div>
+    <main>
+      <Mouse />
+      <motion.div
+        className="contact"
+        initial="out"
+        animate="in"
+        exit="out"
+        variants={pageTransition}
+        transition={{ duration: 0.5 }}
+      >
+        <Navigation />
+        <Logo />
+        <ContactForm />
+        <div className="contact-infos">
+          <div className="address">
+            <div className="content">
+              <h4>adresse</h4>
+              <p>23 avenue Le Nôtre</p>
+              <p>92420 Vaucresson</p>
+            </div>
+          </div>
+          <div className="phone">
+            <div className="content">
+              <h4>téléphone</h4>
+              <CopyToClipboard text="0643655138" className="hover">
+                <p
+                  style={{ cursor: "pointer" }}
+                  className="clipboard"
+                  onClick={() => {
+                    alert("Téléphone copié");
+                  }}
+                >
+                  06 43 65 51 38
+                </p>
+              </CopyToClipboard>
+            </div>
+          </div>
+          <div className="email">
+            <div className="content">
+              <h4>email</h4>
+              <CopyToClipboard text="acetige@gmail.com" className="hover">
+                <p
+                  style={{ cursor: "pointer" }}
+                  className="clipboard"
+                  onClick={() => {
+                    alert("Email copié");
+                  }}
+                >
+                  fchdev@web-agency.com
+                </p>
+              </CopyToClipboard>
+            </div>
+          </div>
+          <SocialNetwork />
+          <div className="credits">
+            <p>FCHDev 2022</p>
+          </div>
+        </div>
+        <Buttons left={"/projet-4"} />
+      </motion.div>
+    </main>
   );
 };
 
